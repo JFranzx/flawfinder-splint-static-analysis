03c7e620a079d4c55e25848d39728b2bb9943175
Francesco <dilorenzofrancesco@outlook.com>
2019-10-30 12:00:44 +0100

    all done

--- output/merged_output/output.c
+++ output/merged_output/output.c
@@ -8,26 +8,32 @@
 
 ssize_t func1()
 {	
-	/* Flawfinder: ignore */
 	char buffer[1024];
   	if(fgets(buffer, 1024, stdin)!=NULL)
   	{
   		if (!isalpha(buffer[0]))
   		{
-     		/* Flawfinder: ignore */
     	 	char errormsg[1044];
      		strlcpy(errormsg, buffer,1024);
-     		// deleted one space char
      		strlcat(errormsg, " is not a valid ID",sizeof(errormsg));
-			fprintf(stderr,"%s\n",errormsg);
-			return -1;
- 		}
-		return 0;
+				fprintf(stderr,"%s\n",errormsg);
+				return -1;
+ 			}
+			
   	}
-  	return -1;
+  
+  	if(ferror(stdin))
+  	{
+  		fprintf(stderr,"%s\n",strerror(errno));
+  		return -1;
+  	}
+  	
+  	return 0;
+  		
+  	
+  	
 }
 
-/*@unused@*/
 ssize_t func2(int f2d)
 {
 	char *buf2 = NULL;
@@ -96,7 +102,7 @@ ssize_t func3(int f3d)
   		return -1; 
   	}
   
-  	buf3 = calloc(len+1,sizeof(char));
+  	buf3 = malloc(len+1,sizeof(char));
   	if(buf3==NULL)
  	{
 		/* error handling */
@@ -132,6 +138,7 @@ int  main()
 	}
 	strlcpy(buffer,foo,sizeof(buffer));
 	ret = func1();
+	exit(0);
 	if(ret < 0)
 	{
 		free(buffer);
